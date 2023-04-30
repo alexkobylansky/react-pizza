c3dfccbf71e6884cc4e71ddfd6e4d676548fb5cf
Alex Kobylansky <alex.kobylansky@gmail.com>
2023-04-30 21:16:30 +0300

    Added fonts, styles and all HTML in App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './global.scss';
-import {App} from './cpmponents/app/App';
+import {App} from './components/app/App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
