05b0e02bc296474f3d5eed2274565668ad23a436
Alex Kobylansky <alex.kobylansky@gmail.com>
2023-05-03 14:10:13 +0300

    Fetched pizzas json file from backend

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import '../../scss/app.scss';
 import {Header} from "../header/Header";
 import {Categories} from "../categories/Categories";
 import {Sort} from "../sort/Sort";
 import {PizzaBlock} from "../pizza-block/PizzaBlock";
-// @ts-ignore
-import pizzas from "../../pizzas.json";
+
+const url: string | undefined = process.env.REACT_APP_BASE_URL;
 
 export const App: React.FC = () => {
+  const firstInit = useRef(false);
+  const [pizzas, setPizzas] = useState<IPizza[]>([]);
+
+  const getPizzas = async (): Promise<void> => {
+    try {
+      const res = await fetch(`${url}/pizzas`);
+      if (res.status == 200) {
+        const req: IPizza[] = await res.json();
+        setPizzas(req);
+      } else if (res.status === 400) {
+        throw new Error('Bad request');
+      } else if (res.status === 404) {
+        throw new Error('Not Found')
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (!firstInit.current) {
+      void getPizzas();
+    }
+    firstInit.current = true;
+  }, []);
+
   return (
     <div className="App">
       <div className="wrapper">
@@ -20,7 +46,7 @@ export const App: React.FC = () => {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {pizzas.map(pizza => <PizzaBlock key={pizza.name} {...pizza}/>)}
+              {pizzas && pizzas.map(pizza => <PizzaBlock key={pizza.name} {...pizza}/>)}
             </div>
           </div>
         </div>
